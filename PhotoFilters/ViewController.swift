350d73edd4c67161bedede0ea0a9287df4fc02a1
Stephanie Li-Diaz <Stephanie@Stephanies-MacBook-Pro.local>
2015-08-18 02:00:01 -0500

    Simple random photo filter prototype completed

--- PhotoFilters/ViewController.swift
+++ PhotoFilters/ViewController.swift
@@ -10,10 +10,37 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var photoImageView: UIImageView!
+    
+    //Create a place to render the filtered image 
+    let context = CIContext(options:nil)
+    @IBAction func applyFilter(sender: AnyObject) {
+        
+        
+        // Create an image to a filter
+        let inputImage = CIImage(image: photoImageView.image)
+        
+        
+        // Create a random color to pass to a filter
+        let randomColor = [kCIInputAngleKey:(Double(arc4random_uniform(314)) / 100) ]
+        
+        //Apply a filter to the image
+        let filteredImage = inputImage.imageByApplyingFilter("CIHueAdjust", withInputParameters: randomColor)
+        
+        
+        //Render the filter image
+        let renderedImage = context.createCGImage(filteredImage, fromRect: filteredImage.extent())
+        
+        //Reflect the change back to the interface
+        photoImageView.image = UIImage(CGImage: renderedImage)
+        
+        
+        
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        println("This will be pushed")
+        println("Testing ")
     }
 
     override func didReceiveMemoryWarning() {
